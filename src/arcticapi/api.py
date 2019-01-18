380f866f81cc66ec8796f0a04029d0bfd03984d0
Yuval <yuval@yuvalboss.com>
2019-01-17 17:46:29 -0800

    forgot debug max images oops

--- src/arcticapi/api.py
+++ src/arcticapi/api.py
@@ -56,8 +56,8 @@ class ArcticApi:
         label_base = cfg.label.split(".")[0]
         chips = []
         for image_path in self.images:
-            # if cfg.debug and len(chips) > 100:
-            #     break
+            if cfg.debug and len(chips) > 100:
+                break
             chips = chips + self.images[image_path].generate_chips(cfg)
 
         random.shuffle(chips)
